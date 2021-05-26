395071e8965c0b4e50e58a57f779f4a66e80d770
Haitham Habchy <hhabchy@gmail.com>
2021-05-26 21:17:40 +0300

    Added libman to use client side library

---
+++ Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public ViewResult GetBook(int id)
+        {
+            var data = _bookRepository.GetBookById(id);
+            return View();
+        }
+
+        public ViewResult SearchBooks(string bookName,string authorName)
+        {
+            var data = _bookRepository.SearchBook(bookName, authorName);
+            return View();
+        }
+    }
+}
